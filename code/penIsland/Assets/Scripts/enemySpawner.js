f277c94180797749e17766d22cf342fbae8e7f80
Joseph Meersman <jmeersman@104-128.sab.du.edu>
2012-01-27 23:13:05 -0700

    phhhhh

---
+++ code/penIsland/Assets/Scripts/enemySpawner.js
@@ -0,0 +1,7 @@
+function Start () {
+  var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+  //cube.AddComponent(Rigidbody);
+  cube.transform.position = Vector3 (11.63709, 3.426198, 11.56675);
+} 
+function Update () {
+}
