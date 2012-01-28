8592bbb5fe07a6aa83c7a721b6813ae9471945b0
Joseph Meersman <jmeersman@052-006.pioneernet.du.edu>
2012-01-28 13:10:42 -0700

    phhhhh

--- code/penIsland/Assets/Scripts/enemyMover.js
+++ code/penIsland/Assets/Scripts/enemyMover.js
@@ -4,6 +4,6 @@ function Update () {
 
  this.transform.position.x -= speed * Time.deltaTime;
  if (this.transform.position.x < -1){
-   //Destroy(GameObject.Find ("Cube(Clone)"));
+   //Destroy(this.gameObject);
  }
 }
--- code/penIsland/Assets/Scripts/enemySpawner.js
+++ code/penIsland/Assets/Scripts/enemySpawner.js
@@ -1,19 +1,19 @@
 var enemyPrefab: Transform;
 var spawnInterval: float = 5.0;
 function Start () {
-  
+    for(i = 0; i < 25; i++){
+      var cube = Instantiate(enemyPrefab, Vector3 (0, 0, 0), Quaternion.identity);
+      var enemyArray = new Array();
+      enemyArray.push(cube);
+      print(enemyArray);
+      /*var top : float = 8.0;
+      var bottom : float = -1.5;
+      var spawnPoint : float = Random.Range(bottom, top);
+      cube.transform.position = Vector3 (12.43211, spawnPoint, 11.56675);*/
+    }
 } 
 var timer: float = 0.0;
 
 function Update () {
   timer = timer + Time.deltaTime;
-  print(timer);
-  if (timer >= spawnInterval){
-    var cube = Instantiate(enemyPrefab, Vector3 (0, 0, 0), Quaternion.identity);
-    var top : float = 8.0;
-    var bottom : float = -1.5;
-    var spawnPoint : float = Random.Range(bottom, top);
-    cube.transform.position = Vector3 (11.63709, spawnPoint, 11.56675);
-    timer = 0.0;
-  }
 }
