7948f26e4637b11e6bc6ae51571f7ba088828193
mojiferous <mojiferous@mojiferous.com>
2012-01-28 17:45:25 -0700

    Poop
    Signed-off-by: mojiferous <mojiferous@mojiferous.com>

--- code/penIsland/Assets/Scripts/ghostMover.js
+++ code/penIsland/Assets/Scripts/ghostMover.js
@@ -1,3 +1,25 @@
+var laserPrefab: Transform; 
+var fired: boolean = false;
+var laserFired : boolean = false;
+var laserSpeed: float = 5;
+var laser;
+
 
 function Update () {
+	if (fired && !laserFired) {
+		laserFire();
+	}
+	if (laserFired) {
+    laser.position.x += laserSpeed * .2;
+    if (laser.position.x >10){
+      Destroy(laser.gameObject);
+      laserFired = false;
+    }
+  }
+}
+
+function laserFire() {
+	fired = false;
+	laserFired = true;
+  laser = Instantiate(laserPrefab, transform.position, transform.rotation);
 }
--- code/penIsland/Assets/Scripts/global.js
+++ code/penIsland/Assets/Scripts/global.js
@@ -5,6 +5,10 @@ var boundLeft : float = -1.03721881;
 var boundRight : float = 11.48046;
 var boundBottom : float = -1.5;
 var boundTop : float = 8.0;
+var locationArray = new Array();
+var ghostBirds = new Array();
+var numGhosts : int;
+var turnNum : int;
 
 function Start() {
   boundRight = 14;
@@ -12,7 +16,55 @@ function Start() {
 }
 
 function Update () {
+	var newTrans : Transform;
+	var isFiring : System.Boolean;
+	
+	newTrans = GameObject.Find("Bird").GetComponent(Transform);
+	isFiring = GameObject.Find("Bird").GetComponent(birdMover).fired; 
+	var serialData = newTrans.localPosition.y+","+isFiring;
+	locationArray.push(serialData);
+	
+	if (ghostBirds.length > 0) {
+		playGhosts();
+	}
+	turnNum++;
+	
+	if (turnNum > 100) {
+		pushToGhosts();
+	}
 }
+
+function pushToGhosts() {
+	//assuming this is called when the player gets to the end
+	//should probably also make a new ghost bird here, or in the bird object itself
+	var tempArray  = new Array();
+	for(var strVar : System.String in locationArray) {
+		tempArray.push(strVar);
+	}
+	ghostBirds.push(tempArray);
+	
+	locationArray.clear();
+	numGhosts++;
+	turnNum = 0;
+}
+
+function playGhosts() {
+	var countGhost : int = 0;
+	
+	for(var ghostObj : GameObject in GameObject.FindGameObjectsWithTag("Ghost")) {
+		
+		var serialData = ghostBirds[countGhost][turnNum];
+		var dataArray = new Array();
+		dataArray = serialData.Split(","[0]);
+		ghostObj.GetComponent(Transform).localPosition.y = float.Parse(dataArray[0]);
+		if (dataArray[1] == "True") {
+			print("Fire!");
+			ghostObj.GetComponent(ghostMover).fired = true;
+		}
+				
+		countGhost++;
+	}
+
 
 /**
  * Utilty function to randomize an array
